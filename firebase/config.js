27aaa6d1b44129b6f46ec54e0b962f6b98da1819
raulDeDevWorld <proyection009@gmail.com>
2022-10-27 14:09:31 -0400

    DC

---
+++ firebase/config.js
@@ -0,0 +1,14 @@
+export const firebaseConfig = {
+
+  apiKey: "AIzaSyA_tsI_bY7j72LAoLwymq2EaeUn2iKD8FQ",
+  authDomain: "sistema-web-d4dde.firebaseapp.com",
+  databaseURL: "https://sistema-web-d4dde-default-rtdb.firebaseio.com",
+  projectId: "sistema-web-d4dde",
+  storageBucket: "sistema-web-d4dde.appspot.com",
+  messagingSenderId: "458796101330",
+  appId: "1:458796101330:web:cec67ad33853d8801f870b"
+  
+  };
+
+
+  
---
+++ layout/Layout.jsx
@@ -0,0 +1,21 @@
+import { handleSignOut } from '../firebase/utils'
+import Button from '../components/Button'
+import style from '../styles/Layout.module.css'
+
+export default function Layout (props) {
+    function logout() {
+        handleSignOut()
+    }
+    return (
+        <>
+            <header className={style.header}>
+                <p>Bienvenido Logistics</p>
+                <Button style='buttonSecondary' click={logout}>
+                    Cerrar Sesion
+                </Button>
+            </header>
+            <main>{props.children}</main>
+        </>
+
+    )
+}
